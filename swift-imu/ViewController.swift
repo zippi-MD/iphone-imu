925dd196877520774a4282d5e5c561fe2660a4e6
Alejandro Mendoza <zippi180@gmail.com>
2019-05-08 13:28:16 -0500

    Initial Commit

---
+++ swift-imu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-imu
+//
+//  Created by Alejandro Mendoza on 5/8/19.
+//  Copyright © 2019 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
